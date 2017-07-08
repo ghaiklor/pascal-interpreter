2804fc987e94e5c1a817e65b16648786ba6c8ad8
Eugene Obrezkov <ghaiklor@gmail.com>
2017-07-08 12:06:37 +0300

    feature(ast): add exports of all new AST nodes

--- src/ast/index.js
+++ src/ast/index.js
@@ -1,11 +1,19 @@
+const Assign = require('./Assign');
 const BinaryOperator = require('./BinaryOperator');
+const Compound = require('./Compound');
 const Node = require('./Node');
+const NoOperation = require('./NoOperation');
 const Number = require('./Number');
 const UnaryOperator = require('./UnaryOperator');
+const Variable = require('./Variable');
 
 module.exports = {
+  Assign,
   BinaryOperator,
+  Compound,
   Node,
+  NoOperation,
   Number,
-  UnaryOperator
+  UnaryOperator,
+  Variable
 };
--- test/ast/index.test.js
+++ test/ast/index.test.js
@@ -3,9 +3,13 @@ const AST = require('../../src/ast');
 
 describe('AST::EntryPoint', () => {
   it('Should properly export all the nodes', () => {
+    assert.isFunction(AST.Assign);
     assert.isFunction(AST.BinaryOperator);
+    assert.isFunction(AST.Compound);
     assert.isFunction(AST.Node);
+    assert.isFunction(AST.NoOperation);
     assert.isFunction(AST.Number);
     assert.isFunction(AST.UnaryOperator);
+    assert.isFunction(AST.Variable);
   });
 });
