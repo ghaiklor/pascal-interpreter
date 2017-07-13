17030f6acc6b51f1359c7748faae68aece2ab30a
Eugene Obrezkov <ghaiklor@gmail.com>
2017-07-13 13:44:07 +0300

    feature(symbols): add a symbol for procedures

---
+++ src/symbols/ProcedureSymbol.js
@@ -0,0 +1,53 @@
+const Symbol = require('./Symbol');
+
+/**
+ * A symbol for procedure units in a program.
+ *
+ * @class
+ * @since 1.0.0
+ */
+class ProcedureSymbol extends Symbol {
+  /**
+   * Creates new procedure symbol instance.
+   *
+   * @param {String} name
+   * @param {Array} params
+   */
+  constructor(name, params = null) {
+    super(name);
+
+    this.params = params;
+  }
+
+  /**
+   * Get params assigned to this procedure symbol.
+   *
+   * @return {Array}
+   */
+  getParams() {
+    return this.params;
+  }
+
+  /**
+   * Returns a string representation of a procedure symbol.
+   *
+   * @return {String}
+   */
+  toString() {
+    return `ProcedureSymbol(${this.name}, ${this.params})`;
+  }
+
+  /**
+   * Static helper for creating procedure symbol.
+   *
+   * @static
+   * @param {String} name
+   * @param {Array} params
+   * @return {ProcedureSymbol}
+   */
+  static create(name, params) {
+    return new this(name, params);
+  }
+}
+
+module.exports = ProcedureSymbol;
---
+++ test/symbols/ProcedureSymbol.test.js
@@ -0,0 +1,25 @@
+const {assert} = require('chai');
+const ProcedureSymbol = require('../../src/symbols/ProcedureSymbol');
+
+describe('Symbols::ProcedureSymbol', () => {
+  it('Should properly instantiate', () => {
+    const symbol = ProcedureSymbol.create('foo');
+
+    assert.instanceOf(symbol, ProcedureSymbol);
+    assert.equal(symbol.name, 'foo');
+    assert.isNull(symbol.params);
+  });
+
+  it('Should properly return params', () => {
+    const symbol = ProcedureSymbol.create('foo', []);
+
+    assert.instanceOf(symbol, ProcedureSymbol);
+    assert.isArray(symbol.getParams());
+  });
+
+  it('Should properly convert into string representation', () => {
+    const symbol = ProcedureSymbol.create('foo', [1, 2]);
+
+    assert.equal(symbol, 'ProcedureSymbol(foo, 1,2)');
+  });
+});
